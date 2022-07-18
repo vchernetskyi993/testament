8ad43805c2b47b9b007e2ae824cfabdd7ceb9d47
Vsevolod Chernetskyi <vchernetskyi993@gmail.com>
2022-07-18 19:22:14 +0300

    testaments non-transferable

--- contracts/Testament.sol
+++ contracts/Testament.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 /**
-    @title Testament contract
-    @notice ERC-1155 implementation. Holds FTs (GOLD, SILVER, BRONZE) and NFTs (TESTAMENT).
-        Anyone can create one (and only one!) testament. On death notice this contract 
-        will split FTs according to the rules defined in owner's testament.
+ * @title Testament contract
+ * @notice ERC-1155 implementation. Holds FTs (GOLD, SILVER, BRONZE) and NFTs (TESTAMENT).
+ *     Anyone can create one (and only one!) testament. On death notice this contract 
+ *     will split FTs according to the rules defined in owner's testament.
  */
 contract Testament is ERC1155 {
   uint256 public constant GOLD = 0;
@@ -32,19 +32,19 @@ contract Testament is ERC1155 {
   }
 
   /**
-    @notice Create new testament for the sender.
-    @param testament testament view with: 
-      1. inheritors - array of addresses that will inherit sender's FTs. 
-      2. shares - array of integers that show how exactly to split posessions.
-        Sum of all shares should be 10000. It means that only 2 decimals proximity is allowed. 
-        For example, if you want to split FTs between 2 accounts by 60.45% and 29.55%, you should send [6045, 2955] shares.
-      3. notifiers - array of addresses that will be able to send death notice (e.g. call `announceExecution`).
-    @dev All arrays should not be empty.
-      Inheritors should have the same length as shares.
-      Shares should sum up to 10000.
-      Testament should not already exist.
-      Emits 'TestementIssued' event.
-  */
+   * @notice Create new testament for the sender.
+   * @param testament testament view with: 
+   *   1. inheritors - array of addresses that will inherit sender's FTs. 
+   *   2. shares - array of integers that show how exactly to split posessions.
+   *     Sum of all shares should be 10000. It means that only 2 decimals proximity is allowed. 
+   *     For example, if you want to split FTs between 2 accounts by 60.45% and 29.55%, you should send [6045, 2955] shares.
+   *   3. notifiers - array of addresses that will be able to send death notice (e.g. call `announceExecution`).
+   * @dev All arrays should not be empty.
+   *   Inheritors should have the same length as shares.
+   *   Shares should sum up to 10000.
+   *   Testament should not already exist.
+   *   Emits 'TestementIssued' event.
+   */
   function issueTestament(TestamentData calldata testament) external {
     require(testament.inheritors.length > 0, "Inheritors should not be empty");
     require(
@@ -74,5 +74,33 @@ contract Testament is ERC1155 {
     return testaments[owner];
   }
 
-  // TODO: disable testament transfers
+  /**
+   * @dev See {IERC1155-safeTransferFrom}.
+   */
+  function safeTransferFrom(
+    address from,
+    address to,
+    uint256 id,
+    uint256 amount,
+    bytes memory data
+  ) public virtual override {
+    require(id != TESTAMENT, "Testaments are non-transferable");
+    super.safeTransferFrom(from, to, id, amount, data);
+  }
+
+  /**
+   * @dev See {IERC1155-safeBatchTransferFrom}.
+   */
+  function safeBatchTransferFrom(
+    address from,
+    address to,
+    uint256[] memory ids,
+    uint256[] memory amounts,
+    bytes memory data
+  ) public virtual override {
+    for (uint256 i = 0; i < ids.length; i++) {
+      require(ids[i] != TESTAMENT, "Testaments are non-transferable");
+    }
+    super.safeBatchTransferFrom(from, to, ids, amounts, data);
+  }
 }
--- test/Testament.ts
+++ test/Testament.ts
@@ -3,40 +3,63 @@ import { expect, use } from "chai";
 import chaiAsPromised from "chai-as-promised";
 import { Testament } from "../typechain-types";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 use(chaiAsPromised);
 
-type EmptyState = {
+type State = {
   contract: Testament;
+  deployer: string;
   issuer: string;
+  issuerSigner: SignerWithAddress;
   inheritors: [string, string, string];
   notifiers: [string];
 };
 
 describe("Testament", () => {
-  async function freshStateFixture(): Promise<EmptyState> {
-    const [_owner, issuer, i0, i1, i2, n0] = await ethers.getSigners();
+  async function freshStateFixture(): Promise<State> {
+    const [deployer, issuer, i0, i1, i2, n0] = await ethers.getSigners();
 
     const factory = await ethers.getContractFactory("Testament");
     const contract = await factory.deploy();
 
     return {
-      contract: await contract.connect(issuer),
+      contract,
       issuer: await issuer.getAddress(),
+      issuerSigner: issuer,
       inheritors: [
         await i0.getAddress(),
         await i1.getAddress(),
         await i2.getAddress(),
       ],
       notifiers: [await n0.getAddress()],
+      deployer: await deployer.getAddress(),
     };
+  }
+
+  async function issuerCallerFixture(): Promise<State> {
+    const state = await freshStateFixture();
+    return {
+      ...state,
+      contract: await state.contract.connect(state.issuerSigner),
+    };
+  }
+
+  async function issuedTestamentFixture(): Promise<State> {
+    const state = await issuerCallerFixture();
+    await state.contract.issueTestament({
+      inheritors: state.inheritors,
+      shares: [4000, 3000, 3000],
+      notifiers: state.notifiers,
+    });
+    return state;
   }
 
   describe("Issue Testament", () => {
     it("Should issue testament", async () => {
       // given
       const { contract, issuer, inheritors, notifiers } = await loadFixture(
-        freshStateFixture
+        issuerCallerFixture
       );
       const input = {
         inheritors,
@@ -63,12 +86,7 @@ describe("Testament", () => {
 
     it("Should check inheritors non empty", async () => {
       // given
-      const {
-        contract,
-        issuer: _issuer,
-        inheritors: _inheritors,
-        notifiers,
-      } = await loadFixture(freshStateFixture);
+      const { contract, notifiers } = await loadFixture(issuerCallerFixture);
       const input = {
         inheritors: [],
         shares: [4000, 3000, 3000],
@@ -83,12 +101,9 @@ describe("Testament", () => {
 
     it("Inheritors and shares should have equal sizes", async () => {
       // given
-      const {
-        contract,
-        issuer: _issuer,
-        inheritors,
-        notifiers,
-      } = await loadFixture(freshStateFixture);
+      const { contract, inheritors, notifiers } = await loadFixture(
+        issuerCallerFixture
+      );
       const input = {
         inheritors,
         shares: [4000, 3000],
@@ -103,12 +118,9 @@ describe("Testament", () => {
 
     it("Should check shares total is 100%", async () => {
       // given
-      const {
-        contract,
-        issuer: _issuer,
-        inheritors,
-        notifiers,
-      } = await loadFixture(freshStateFixture);
+      const { contract, inheritors, notifiers } = await loadFixture(
+        issuerCallerFixture
+      );
       const input = {
         inheritors,
         shares: [4000, 3000, 2500],
@@ -123,11 +135,7 @@ describe("Testament", () => {
 
     it("Should check notifiers not empty", async () => {
       // given
-      const {
-        contract,
-        issuer: _issuer,
-        inheritors,
-      } = await loadFixture(freshStateFixture);
+      const { contract, inheritors } = await loadFixture(issuerCallerFixture);
       const input = {
         inheritors,
         shares: [4000, 3000, 3000],
@@ -142,12 +150,9 @@ describe("Testament", () => {
 
     it("Should check testament isn't already present", async () => {
       // given
-      const {
-        contract,
-        issuer: _issuer,
-        inheritors,
-        notifiers,
-      } = await loadFixture(freshStateFixture);
+      const { contract, inheritors, notifiers } = await loadFixture(
+        issuerCallerFixture
+      );
       const input = {
         inheritors,
         shares: [4000, 3000, 3000],
@@ -165,12 +170,82 @@ describe("Testament", () => {
   });
 
   describe("Transfer Testament", () => {
-    it("Should transfer FTs", () => {});
+    it("Should transfer FTs", async () => {
+      // given
+      const { contract, deployer, issuer } = await loadFixture(
+        freshStateFixture
+      );
+      const amount = 1000;
 
-    it("Should batch transfer FTs", () => {});
+      // when
+      await contract.safeTransferFrom(
+        deployer,
+        issuer,
+        contract.GOLD(),
+        amount,
+        []
+      );
 
-    it("Should not transfer testament", () => {});
+      // then
+      const possession = await contract.balanceOf(issuer, contract.GOLD());
+      expect(possession.toNumber()).to.be.equal(amount);
+    });
 
-    it("Should not batch transfer testaments", () => {});
+    it("Should batch transfer FTs", async () => {
+      // given
+      const { contract, deployer, issuer } = await loadFixture(
+        freshStateFixture
+      );
+      const bronzeAmount = 9000;
+      const goldAmount = 1000;
+
+      // when
+      await contract.safeBatchTransferFrom(
+        deployer,
+        issuer,
+        [contract.BRONZE(), contract.GOLD()],
+        [bronzeAmount, goldAmount],
+        []
+      );
+
+      // then
+      const goldPossession = await contract.balanceOf(issuer, contract.GOLD());
+      expect(goldPossession.toNumber()).to.be.equal(goldAmount);
+      const bronzePossession = await contract.balanceOf(
+        issuer,
+        contract.BRONZE()
+      );
+      expect(bronzePossession.toNumber()).to.be.equal(bronzeAmount);
+    });
+
+    it("Should not transfer testament", async () => {
+      // given
+      const { contract, deployer, issuer } = await loadFixture(
+        issuedTestamentFixture
+      );
+
+      // when+then
+      return expect(
+        contract.safeTransferFrom(issuer, deployer, contract.TESTAMENT(), 1, [])
+      ).to.be.rejectedWith(/non-transferable/);
+    });
+
+    it("Should not batch transfer testaments", async () => {
+      // given
+      const { contract, deployer, issuer } = await loadFixture(
+        issuedTestamentFixture
+      );
+
+      // when+then
+      return expect(
+        contract.safeBatchTransferFrom(
+          issuer,
+          deployer,
+          [contract.TESTAMENT()],
+          [1],
+          []
+        )
+      ).to.be.rejectedWith(/non-transferable/);
+    });
   });
 });
